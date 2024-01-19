903f150e203c1cfe93ad69da1c8e0ff56f837b1b
howardwkh <howardwan_8887@yahoo.com>
2024-01-19 21:51:48 +0800

    Header will change to show the station name when a station is chosen

--- src/MTRContainer.js
+++ src/MTRContainer.js
@@ -13,13 +13,13 @@ export default function MTRContainer() {
     const mtr_stations = mtr_station_json.data.station;
 
     let [selectedRoute, setSelectedRoute] = useState(['', -1]);
-    let [selectedStation, setSelectedStation] = useState(['', -1]);
+    let [selectedStation, setSelectedStation] = useState(['', -1, '']);
 
     const scrollToRef = useRef(null);
 
     useEffect(() => {
         if (scrollToRef.current) {
-            scrollToRef.current.scrollIntoView({ behavior: 'smooth', inline: 'start' });
+            scrollToRef.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'start' });
         }
     }, [selectedStation]);
 
@@ -27,9 +27,9 @@ export default function MTRContainer() {
         setSelectedRoute((selectedRoute[0] === route) ? ['', -1] : [route, index]);
     }
 
-    function selectStation (station, index) {
+    function selectStation (station, index, name) {
         if (station !== selectedStation[0]) {
-            setSelectedStation([station, index]);
+            setSelectedStation([station, index, name]);
         }
     }
 
@@ -43,7 +43,7 @@ export default function MTRContainer() {
                 <div className="scroll_x scroll_bar-1" hidden={route.code !== selectedRoute[0]}>
                     <div className="grid-6-minmax grid-column">
                         {mtr_stations[index].map((station) => (
-                            <div onClick={() => selectStation(station.code, station.seq)}>
+                            <div onClick={() => selectStation(station.code, station.seq, station.name)}>
                                 <MTRStations key={route.code+station.code} lineColor={route.code} lines={station.lines} name={station.name} lineName={station.lineName} lineStyle={station.style} />
                             </div>))}
                     </div>
@@ -55,13 +55,13 @@ export default function MTRContainer() {
             <div className="grid-3-fixed">
                 <h2 className={`button_base color_${selectedRoute[0]}`}>{mtr_routes[selectedRoute[1]].name}</h2>
                 <h1>{mtr_stations[selectedRoute[1]][selectedStation[1] - 1].name}</h1>
-                <button className='button_base button_hover' onClick={() => setSelectedStation(['', -1])}>返回</button>
+                <button className='button_base button_hover' onClick={() => setSelectedStation(['', -1, ''])}>返回</button>
             </div>
 
             <div className="scroll_x scroll_bar-1">
                 <div className="grid-6-minmax grid-column">
                     {mtr_stations[selectedRoute[1]].map((station) => (
-                        <div ref={(station.seq !== Math.max(selectedStation[1]-1, 0) ) ? null : scrollToRef} onClick={() => selectStation(station.code, station.seq)}>
+                        <div ref={(station.seq !== Math.max(selectedStation[1]-1, 0) ) ? null : scrollToRef} onClick={() => selectStation(station.code, station.seq, station.name)}>
                             <MTRStations key={selectedRoute[0].code+station.code} lineColor={selectedRoute[0]} lines={station.lines} name={station.name} lineName={station.lineName} lineStyle={station.style} selected={station.seq === selectedStation[1]}/>
                         </div>
                     ))}
@@ -71,10 +71,17 @@ export default function MTRContainer() {
         </>;
     }
 
+    let headersToRender;
+    if (selectedStation[0] === '') {
+        headersToRender = <Header text="你喺邊個地鐵站？" />;
+    } else {
+        headersToRender = <Header text={selectedStation[2] + " 幾點有車？"} />;
+    }
+
     return (
         <div className="container">
             <div className="container_top">
-                <Header text="你喺邊個地鐵站？" />
+                {headersToRender}
             </div>
             <div className="container_mid scroll_bar-1">
                 {componentsToRender}
